de7f54bbe97db3e1c1d66ace5629eeafed724b61
Otávio Campezzi <90792115+OCampezzi@users.noreply.github.com>
2024-11-07 18:00:00 -0300

    js wip

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const input = document.getElementById('guess');
+const submit = document.getElementById('check');
+const score = document.getElementById('score');
+const message = document.getElementById('message');
+const againBtn = document.getElementById('again');
+
+// function to generate the secret number
+function getRandomNum(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled)) + minCeiled;
+}
+
+const secretNumber = getRandomNum(1, 20);
+const highScore = 0;
+
+submit.addEventListener('click', function () {
+  if (input.value === '') {
+    message.textContent = 'Please, enter a number!';
+  } else if (input.value > secretNumber && input.value <= 20) {
+    message.textContent = 'Too high!';
+    score.textContent = Number(score.textContent) - 1;
+  } else if (input.value < secretNumber && input.value >= 1) {
+    message.textContent = 'Too low!';
+    score.textContent = Number(score.textContent) - 1;
+  } else if (input.value == secretNumber) {
+    message.textContent = '🎉 Congratulations, you guessed it right!';
+    highScore = score.textContent;
+  } else {
+    message.textContent = 'Please, enter a number between 1 and 20!';
+  }
+});
